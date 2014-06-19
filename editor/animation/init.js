533e4ee090cc0e7a156cc66c2828c4b6259f9e12
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-06-19 16:27:35 +1200

    animation

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -39,14 +39,16 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
                 return false;
             }
 
-            var default_in = [[0, 1, 0, 1, 0, 0],
-                      [1, 8, 1, 0, 0, 0],
-                      [0, 1, 2, 0, 0, 1],
-                      [1, 0, 0, 1, 1, 0],
-                      [0, 0, 0, 1, 3, 1],
-                      [0, 0, 1, 0, 1, 2]];
+            var default_in = [
+                [0, 1, 0, 1, 0, 0],
+                [1, 8, 1, 0, 0, 0],
+                [0, 1, 2, 0, 0, 1],
+                [1, 0, 0, 1, 1, 0],
+                [0, 0, 0, 1, 3, 1],
+                [0, 0, 1, 0, 1, 2]
+            ];
             var checkioInput = data.in || default_in;
-            var checkioInputStr = 'capture(' + JSON.stringify(checkioInput) + ')';
+            var checkioInputStr = 'capture([<br>' + checkioInput.map(JSON.stringify).join(",<br>") + ',<br>])';
 
             var failError = function (dError) {
                 $content.find('.call').html('Fail: ' + checkioInputStr);
@@ -95,7 +97,8 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
 
             if (explanation) {
                 var canvas = new Network();
-                canvas.draw($content.find(".explanation")[0], explanation, checkioInput[0], checkioInput[1], checkioInput[2]);
+                canvas.draw($content.find(".explanation")[0], checkioInput);
+                canvas.animate(explanation, checkioInput);
             }
 
 
@@ -142,62 +145,89 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
             options = options || {};
 
             var R = options.radius || 160;
-            var objR = 15;
-            var w = 25;
-            var h = 10;
+            var objR = 20;
+//            var w = 25;
+//            var h = 10;
 
             var x0 = 10;
 
-            var sizeX = 2 * x0 + 2 * (R + w);
-            var sizeY = 2 * x0 + 2 * (R + h);
+            var sizeX = 2 * x0 + 2 * (R + objR);
+            var sizeY = 2 * x0 + 2 * (R + objR);
 
-            var centerX = x0 + R + w;
-            var centerY = x0 + R + h;
+            var centerX = x0 + R + objR;
+            var centerY = x0 + R + objR;
 
 
             var paper;
             var networkObjects = {};
+            var networkLines = {};
+
+            var stepTime = 300;
 
             var attrCircle = {"stroke": colorBlue4, "stroke-width": 2, "fill": colorBlue1};
-            var attrRect = {"stroke": colorBlue4, "stroke-width": 2, "fill": colorBlue1};
+//            var attrRect = {"stroke": colorBlue4, "stroke-width": 2, "fill": colorBlue1};
             var attrNumber = {"font-family": "Roboto, Verdana, Geneva, sans-serif", "font-size": objR * 1.5};
-            var attrName = {"font-family": "Roboto, Verdana, Geneva, sans-serif", "font-size": h};
-            var attrLine = {"stroke": colorOrange4, "stroke-width": 3};
+//            var attrName = {"font-family": "Roboto, Verdana, Geneva, sans-serif", "font-size": h};
+            var attrLine = {"stroke": colorBlue2, "stroke-width": 5};
 
-            this.draw = function(dom, matrix) {
+            this.draw = function (dom, matrix) {
                 paper = Raphael(dom, sizeX, sizeY);
                 var angle = Math.PI * 2 / matrix.length;
                 for (var i = 0; i < matrix.length; i++) {
                     var obj = paper.set();
                     var x = centerX - Math.cos(i * angle) * R;
                     var y = centerY - Math.sin(i * angle) * R;
-                    obj.push(paper.circle(x, y, objR).attr(attrCircle));
+                    obj.push(paper.circle(x, y, objR).attr(attrCircle).attr("stroke-width", matrix[i][i]));
 //                    obj.push(paper.circ(x - w, y - h, objR).attr(attrRect));
 
 
                     if (i === 0) {
-                        obj[0].attr("fill", colorOrange1);
+                        obj[0].attr("fill", colorOrange4);
                     }
                     obj.push(paper.text(x, y, i).attr(attrNumber));
 //                    obj.push(paper.text(x, y, names[i]).attr(attrName));
                     obj.x = x;
                     obj.y = y;
                     networkObjects[i] = obj;
+                    networkLines[i] = [];
                 }
-//                for (i = 0; i < matrix.length; i++) {
-//                    var connection = network[i].split("-");
-//                    var fr = networkObjects[connection[0]];
-//                    var to = networkObjects[connection[1]];
-//                    paper.path(
-//                        format("M{0},{1}L{2},{3}",
-//                            fr.x,
-//                            fr.y,
-//                            to.x,
-//                            to.y)).attr(attrLine).toBack();
-//
-//                }
-            }
+                for (i = 0; i < matrix.length; i++) {
+                    for (var j = i + 1; j < matrix.length; j++) {
+                        if (matrix[i][j] === 0) {
+                            continue;
+                        }
+                        var fr = networkObjects[i];
+                        var to = networkObjects[j];
+                        var p = paper.path(
+                            format("M{0},{1}L{2},{3}",
+                                fr.x,
+                                fr.y,
+                                to.x,
+                                to.y)).attr(attrLine).toBack();
+                        networkLines[i].push(p);
+                        networkLines[j].push(p);
+                        if (i === 0) {
+                            p.attr("stroke", colorOrange4);
+                        }
+                    }
+                }
+            };
 
+            this.animate = function(expl, matrix) {
+                for (var k = 0; k < matrix.length; k++) {
+                    setTimeout(function(node) {
+                        return function() {
+                            networkObjects[node].animate({"stroke-width": 0}, stepTime * matrix[node][node],
+                            callback=function(){
+                                networkObjects[node][0].attr("fill", colorOrange4);
+                                for (var n = 0; n < networkLines[node].length; n++) {
+                                    networkLines[node][n].attr("stroke", colorOrange4);
+                                }
+                            });
+                        }
+                    }(k), expl[k] * stepTime);
+                }
+            }
         }
 
         //Your Additional functions or objects inside scope
