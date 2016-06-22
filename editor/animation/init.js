30f505483c7082772f6597817d468184411e1111
Oleksandr Liabakh <oleksandrliabakh@Oleksandrs-MacBook.local>
2016-06-22 17:49:40 +0300

    ext_editor_io

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -1,124 +1,7 @@
 //Dont change it
-requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
-    function (ext, $, TableComponent) {
+requirejs(['ext_editor_io', 'jquery_190', 'raphael_210'],
+    function (extIO, $, TableComponent) {
 
-        var cur_slide = {};
-
-        ext.set_start_game(function (this_e) {
-        });
-
-        ext.set_process_in(function (this_e, data) {
-            cur_slide["in"] = data[0];
-        });
-
-        ext.set_process_out(function (this_e, data) {
-            cur_slide["out"] = data[0];
-        });
-
-        ext.set_process_ext(function (this_e, data) {
-            cur_slide.ext = data;
-            this_e.addAnimationSlide(cur_slide);
-            cur_slide = {};
-        });
-
-        ext.set_process_err(function (this_e, data) {
-            cur_slide['error'] = data[0];
-            this_e.addAnimationSlide(cur_slide);
-            cur_slide = {};
-        });
-
-        ext.set_animate_success_slide(function (this_e, options) {
-            var $h = $(this_e.setHtmlSlide('<div class="animation-success"><div></div></div>'));
-            this_e.setAnimationHeight(115);
-        });
-
-        ext.set_animate_slide(function (this_e, data, options) {
-            var $content = $(this_e.setHtmlSlide(ext.get_template('animation'))).find('.animation-content');
-            if (!data) {
-                console.log("data is undefined");
-                return false;
-            }
-
-            var default_in = [
-                [0, 1, 0, 1, 0, 0],
-                [1, 8, 1, 0, 0, 0],
-                [0, 1, 2, 0, 0, 1],
-                [1, 0, 0, 1, 1, 0],
-                [0, 0, 0, 1, 3, 1],
-                [0, 0, 1, 0, 1, 2]
-            ];
-            var checkioInput = data.in || default_in;
-            var checkioInputStr = 'capture([<br>' + checkioInput.map(JSON.stringify).join(",<br>") + ',<br>])';
-
-            var failError = function (dError) {
-                $content.find('.call').html('Fail: ' + checkioInputStr);
-                $content.find('.output').html(dError.replace(/\n/g, ","));
-
-                $content.find('.output').addClass('error');
-                $content.find('.call').addClass('error');
-                $content.find('.answer').remove();
-                $content.find('.explanation').remove();
-                this_e.setAnimationHeight($content.height() + 60);
-            };
-
-            if (data.error) {
-                failError(data.error);
-                return false;
-            }
-
-            if (data.ext && data.ext.inspector_fail) {
-                failError(data.ext.inspector_result_addon);
-                return false;
-            }
-
-            var rightResult = data.ext["answer"];
-            var userResult = data.out;
-            var result = data.ext["result"];
-            var result_addon = data.ext["result_addon"];
-
-
-            //if you need additional info from tests (if exists)
-            var explanation = data.ext["explanation"];
-
-            $content.find('.output').html('&nbsp;Your result:&nbsp;' + JSON.stringify(userResult));
-
-            if (!result) {
-                $content.find('.call').html('Fail: ' + checkioInputStr);
-                $content.find('.answer').html('Right result:&nbsp;' + JSON.stringify(rightResult));
-                $content.find('.answer').addClass('error');
-                $content.find('.output').addClass('error');
-                $content.find('.call').addClass('error');
-            }
-            else {
-                $content.find('.call').html('Pass: ' + checkioInputStr);
-                $content.find('.answer').remove();
-            }
-            //Dont change the code before it
-
-            if (explanation) {
-                var canvas = new Network();
-                canvas.draw($content.find(".explanation")[0], checkioInput);
-                canvas.animate(explanation, checkioInput);
-            }
-
-
-            this_e.setAnimationHeight($content.height() + 60);
-
-        });
-
-        //This is for Tryit (but not necessary)
-//        var $tryit;
-//        ext.set_console_process_ret(function (this_e, ret) {
-//            $tryit.find(".checkio-result").html("Result<br>" + ret);
-//        });
-//
-//        ext.set_generate_animation_panel(function (this_e) {
-//            $tryit = $(this_e.setHtmlTryIt(ext.get_template('tryit'))).find('.tryit-content');
-//            $tryit.find('.bn-check').click(function (e) {
-//                e.preventDefault();
-//                this_e.sendToConsoleCheckiO("something");
-//            });
-//        });
 
         function Network(options) {
 
@@ -235,6 +118,24 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
         //
         //
 
+        var io = new extIO({
+            animation: function($expl, data){
+                var checkioInput = data.in;
+                var explanation = data.ext?data.ext.explanation:undefined;
+                if (!checkioInput || !explanation) {
+                    return;
+                }
+                var canvas = new Network();
+                canvas.draw($content.find($expl[0], checkioInput);
+                canvas.animate(explanation, checkioInput);
+                
+            },
+            functions: {
+                python: 'capture',
+                js: 'capture'
+            }
+        });
+        io.start();
 
     }
 );
